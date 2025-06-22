53c782426583a83fbd06b3f7d9f0807021383af5
hakim-asrori <saneglos005@gmail.com>
2025-06-22 14:57:48 +0700

    update cors and permission

---
+++ database/migrations/2025_06_22_142451_add_column_parent_id_in_permissions.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->foreignIdFor(Permission::class, 'parent_id')->nullable()->references('id')->on('permissions')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->dropForeignIdFor(Permission::class, 'parent_id');
+        });
+    }
+};
