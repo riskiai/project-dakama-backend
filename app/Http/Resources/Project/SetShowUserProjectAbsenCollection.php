180e58c2ea5e863f6b08105772b7a0eb25c6e1a4
BerbagiTeknologi <berbagiteknologi.net@gmail.com>
2025-08-28 19:17:42 +0700

    update

--- app/Http/Resources/Project/SetShowUserProjectAbsenCollection.php
+++ app/Http/Resources/Project/SetShowUserProjectAbsenCollection.php
@@ -10,16 +10,28 @@ class SetShowUserProjectAbsenCollection extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
-            'user'       => [
+            'id'   => $this->id,
+            'user' => [
                 'id'   => $this->user_id,
                 'name' => $this->user->name ?? null,
+
+                'role' => [
+                    'id'   => optional($this->user)->role_id,
+                    'name' => optional(optional($this->user)->role)->role_name,
+                ],
+
+                'divisi' => [
+                    'id'   => optional($this->user)->divisi_id,
+                    'name' => optional(optional($this->user)->divisi)->name,
+                ],
             ],
-            'project'    => [
+
+            'project' => [
                 'id'   => $this->project_id,
                 'name' => $this->project->name ?? null,
             ],
-            'location'   => $this->location,
+
+            'location' => $this->location,
         ];
     }
 }
